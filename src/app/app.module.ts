e3a5cc66ed1f55e33ec0b699d3caac20b3a6a6f4
Fernan <36297944+FerRodSan@users.noreply.github.com>
2019-01-25 21:31:32 +0100

    confirm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { CDVPhotoLibraryPipe } from './cdvphotolibrary.pipe';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { Camera } from '@ionic-native/camera';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { PhotoLibrary } from '@ionic-native/photo-library';
+import { PermissionsPage } from '../pages/permissions/permissions';
+import { ItemDetailsPage } from '../pages/item-details/item-details';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,PermissionsPage,
+    ItemDetailsPage,CDVPhotoLibraryPipe
   ],
   imports: [
     BrowserModule,
@@ -19,11 +24,12 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,PermissionsPage,
+    ItemDetailsPage
   ],
   providers: [
     StatusBar,
-    SplashScreen,
+    SplashScreen,Camera, PhotoLibrary,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,114 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { NavController, ModalController, ToastController, Platform } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { PhotoLibrary, LibraryItem } from '@ionic-native/photo-library';
+import { PermissionsPage } from '../permissions/permissions';
+import { ItemDetailsPage } from '../item-details/item-details';
+
+const THUMBNAIL_WIDTH = 512;
+const THUMBNAIL_HEIGHT = 384;
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  //selector: 'app',
+  template: '<img [src]="url | cdvphotolibrary">'
 })
 export class HomePage {
+  public url: string = 'placeholder.jpg';
+  myphoto: any;
+  thumbnailWidth = THUMBNAIL_WIDTH + 'px';
+  thumbnailHeight = THUMBNAIL_HEIGHT + 'px';
+  library: LibraryItem[];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private camera: Camera, private photoLibrary: PhotoLibrary, private platform: Platform, private cd: ChangeDetectorRef,
+    private toastCtrl: ToastController, private modalCtrl: ModalController) {
+    this.library = [];
+    //this.url = libraryItem.thumbnailURL;    Esto da error
+  }
 
+  takePhoto() {
+    const options: CameraOptions = {
+      quality: 70,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.myphoto = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+    });
+  }
+
+  getImage() {
+    const options: CameraOptions = {
+      quality: 70,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      saveToPhotoAlbum: false
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.myphoto = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+    });
+  }
+
+  cropImage() {
+    const options: CameraOptions = {
+      quality: 70,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      saveToPhotoAlbum: false,
+      allowEdit: true,
+      targetWidth: 300,
+      targetHeight: 300
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.myphoto = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+    });
+  }
+
+  fetchPhotos() {
+    this.platform.ready().then(() => {
+      this.library = [];
+      this.photoLibrary.getLibrary({ thumbnailWidth: THUMBNAIL_WIDTH, thumbnailHeight: THUMBNAIL_HEIGHT}).subscribe({
+        next: (chunk) => {
+          this.library = this.library.concat(chunk);
+          console.log(this.library);
+          this.cd.detectChanges();
+        },
+        error: (err: string) => {
+          if (err.startsWith('Permission')) {
+            let permissionsModal = this.modalCtrl.create(PermissionsPage);
+            permissionsModal.onDidDismiss(() => {
+              this.fetchPhotos();
+            });
+            permissionsModal.present();
+          } else {
+            let toast = this.toastCtrl.create({
+              message: `getLibrary error: ${err}`,
+              duration: 6000,
+            });
+            toast.present();
+          }
+        },
+        complete: () => { }
+      });
+    });
+  }
+
+  itemTapped(event, libraryItem) {
+    this.navCtrl.push(ItemDetailsPage, {
+      libraryItem: libraryItem
+    });
+  }
+
+  trackById(index: number, libraryItem: LibraryItem): string {
+    return libraryItem.id;
   }
 
 }
